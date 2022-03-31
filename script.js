e73aacba840b4c6e5ea62a0abc810809d4f58f5d
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-03-30 19:25:19 -0500

    added characters to include prompts, added a check to make sure at least 1 prompt was selected

--- script.js
+++ script.js
@@ -15,19 +15,34 @@ generateBtn.addEventListener("click", writePassword);
 // The above listens for a click, then obtains a password and assigns the value of that password to the <textarea>
 //need a function that generates a password based on the criteria ()
 function generatePassword() {
-// Need a series of prompts asking for password criteria. User can select these criteria
-// Prompted Criteria: lowercase, uppercase, numeric, and/or special chacters
 // A prompted criteria must include length between 8-128 characters 
 // When a criteria is selected, the input must be validated "Will include lowercase" "Not including lowercase"
 // Then password is generated based off of the prompts
 
-// .confirm() alerts for each criteria
+// Prompt Variables: lowercase, uppercase, numeric, and/or special chacters
+/* var lowerBoolean = false;
+var upperBoolean = false;
+var numberBoolean = false;
+var specialBoolean = false; */
 
-// prompt values are booleans
+// User input confirming character types to include and storing the response: 
+/* lowerBoolean = window.confirm("Do you want lowercase chacaters in password?")
+upperBoolean = window.confirm("Do you want uppercase chacaters in password?")
+numberBoolean = window.confirm("Do you want number chacaters in password?")
+specialBoolean = window.confirm("Do you want special chacaters in password?") */
 
 // If all criteria (not including character) = false display alert "Must select Criteria. Please select Generate again" return;
+/* if(!lowerBoolean && !upperBoolean && !numberBoolean && !specialBoolean) {
+  window.alert("Must select at least one criteria. Please select 'Generate Password' to try again.");
+  return;
+} */
 
-// while loop that checks if charcter limit selected is between 8-128
+// prompts for how many characters and stores that value
+var charNumber = 0;
+charNumber = window.prompt("How many characters would you like your password to be?\n Please enter any whole number value between 8-128:");
+
+
+
 
 
 
